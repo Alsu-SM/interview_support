4e2e9cace5208431c9a1807cb9087c6c31c2e393
Alsu Shareeva <alsu1shareeva@gmail.com>
2025-11-15 16:07:12 +0300

    .

---
+++ src/Components/Icons/IconCheck.tsx
@@ -0,0 +1,28 @@
+import { cssIcon } from './styles';
+import { IIconProps } from './types';
+import styled from '@emotion/styled';
+
+const IconCheckUnstyled = ({ className }: IIconProps) => {
+	return (
+		<svg
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+			className={className}
+		>
+			<path
+				d="M19.5 7L9 17.5L5 13.5"
+				stroke="black"
+				stroke-width="2"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			/>
+		</svg>
+	);
+};
+
+IconCheckUnstyled.displayName = 'IconCheckUnstyled';
+
+export const IconCheck = styled(IconCheckUnstyled)(cssIcon);
